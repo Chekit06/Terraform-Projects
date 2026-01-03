ae800d5f938e927c100dc49e97da15cff69c1e1d
Chekit Sharma <chekitsharma@Chekits-MacBook-Air.local>
2026-01-03 17:00:17 +0530

    remote backend setup

---
+++ remote-state-configuration/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "chekit-terraform-state-bucket-2026"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt = true
+    
+  }
+}
---
+++ remote-state-configuration/main.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "sg" {
+  name = "Basic-Security-Group"
+  description = "Allow port 80 for http"
+
+  tags = {
+    name = "Basic-sg"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "eggress-rule" {
+    security_group_id = aws_security_group.sg.id
+    cidr_ipv4 = "0.0.0.0/0"
+    ip_protocol = "-1"
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ingress-rule" {
+    security_group_id = aws_security_group.sg.id
+    cidr_ipv4 = "0.0.0.0/0"
+    from_port = 80
+    to_port = 80
+    ip_protocol = "tcp"
+}
+
+resource "aws_instance" "web" {
+    ami = "ami-0ecb62995f68bb549"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.sg.id]
+
+    tags = {
+      Name = "basic-terra" 
+
+}  
+}
+
+output "instance_public_ip" {
+    value = aws_instance.web.public_ip
+    description = "The public ip of instance:"
+  
+}
